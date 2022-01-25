3cdb2bbe402aaea63bc5535fcc8b26fa5ad95346
mokhy2 <91274441+mokhy2@users.noreply.github.com>
2022-01-25 15:21:52 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 5,
     center: { lat: 50.369, lng: -81.669 },
   });
   // Create an array of alphabetical characters used to label the markers.
