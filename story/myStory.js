76ae55a218f6ed7cebc144f3fbf1c5f1e2e1b35e
mokhy2 <alimokheam@outlook.com>
2022-02-02 15:36:15 -0500

    Test 11

--- story/myStory.js
+++ story/myStory.js
@@ -1,38 +1,37 @@
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("conservMap"), {
-        zoom: 10,
-        center: { lat: 44.170, lng: -78.609 },
-      });
-      
-      var convAreas = [
-        [{ lat: 43.8519712, lng: -78.9583349 }, "Lynde Shores Conservation Area"],
-        [{ lat: 43.9392489, lng: -78.9811295 }, "Heber Down Conservation Area"],
-        [{ lat: 43.853590, lng: -78.898358 }, "Thicksons Woods"],
-        [{ lat: 44.009224, lng: -79.070157 }, "Glen Major"],
-        [{ lat: 44.002296, lng: -78.774953 }, "Enniskillen Conservation Area"],
-        [{ lat: 43.900200, lng: -79.065092 }, "Greenwood Conservation Area"],
-        [{ lat: 44.297645, lng: -78.270045 }, "Mark S. Burnham Provincial Park"],
-        [{ lat: 44.321988, lng: -78.261056 }, "Harold Town Conservation Area"],
-      ];
-      // Create an info window to share between markers.
-      var infoWindow = new google.maps.InfoWindow();
-    
-      // Create the markers.
-      convAreas.forEach(([position, title], i) => {
-        var marker = new google.maps.Marker({
-          position,
-          map,
-          animation: google.maps.Animation.DROP,
-          title: `${i + 1}. ${title}`,
-          label: `${i + 1}`,
-          optimized: false,
-        });
-    
-        // Add a click listener for each marker, and set up the info window.
-        marker.addListener("click", () => {
-          infoWindow.close();
-          infoWindow.setContent(marker.getTitle());
-          infoWindow.open(marker.getMap(), marker);
-        });
-      });
+  const map = new google.maps.Map(document.getElementById("conservMap"), {
+    zoom: 10,
+    center: { lat: 44.17, lng: -78.609 },
+  });
+
+  var convAreas = [
+    [{ lat: 43.8519712, lng: -78.9583349 }, "Lynde Shores Conservation Area"],
+    [{ lat: 43.9392489, lng: -78.9811295 }, "Heber Down Conservation Area"],
+    [{ lat: 43.85359, lng: -78.898358 }, "Thicksons Woods"],
+    [{ lat: 44.009224, lng: -79.070157 }, "Glen Major"],
+    [{ lat: 44.002296, lng: -78.774953 }, "Enniskillen Conservation Area"],
+    [{ lat: 43.9002, lng: -79.065092 }, "Greenwood Conservation Area"],
+    [{ lat: 44.297645, lng: -78.270045 }, "Mark S. Burnham Provincial Park"],
+    [{ lat: 44.321988, lng: -78.261056 }, "Harold Town Conservation Area"],
+  ];
+  // Create an info window to share between markers.
+  var infoWindow = new google.maps.InfoWindow();
+
+  // Create the markers.
+  convAreas.forEach(([position, title], i) => {
+    var marker = new google.maps.Marker({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      label: `${i + 1}`,
+      optimized: false,
+    });
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+    });
+  });
 }
