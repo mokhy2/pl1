1184255dc51792a4e6fa513c9a601fd325a48280
mokhy2 <alimokheam@outlook.com>
2022-02-02 17:05:45 -0500

    Major Test 19

--- story/myStory.js
+++ story/myStory.js
@@ -3,24 +3,4 @@ function initMap() {
     zoom: 10,
     center: { lat: 44.17, lng: -78.609 },
   });
-
-  var convAreas = [
-    { lat: 43.8519712, lng: -78.9583349 },
-    { lat: 43.9392489, lng: -78.9811295 },
-    { lat: 43.85359, lng: -78.898358 },
-    { lat: 44.009224, lng: -79.070157 },
-    { lat: 44.002296, lng: -78.774953 },
-    { lat: 43.9002, lng: -79.065092 },
-    { lat: 44.297645, lng: -78.270045 },
-    { lat: 44.321988, lng: -78.261056 },
-  ];
-  // Create an info window to share between markers.
-  var infoWindow = new google.maps.InfoWindow();
-
-  // // Create the markers.
-  convAreas.map((location) => {
-    return new google.maps.Marker({
-      position: location
-    });
-  });
 }
