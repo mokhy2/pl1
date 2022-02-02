88c534b3d98f42c6b25044ea5fd308811ee7c8cf
mokhy2 <alimokheam@outlook.com>
2022-02-02 17:11:25 -0500

    Map

--- story/myStory.js
+++ story/myStory.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("conservMap"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 44.17, lng: -78.609 },
   });
