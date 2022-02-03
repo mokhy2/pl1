f200ad29fc15e529a5c52d50b5e16e1bbbdc07f2
mokhy2 <alimokheam@outlook.com>
2022-02-02 19:05:37 -0500

    Commit

--- story/myStory.js
+++ story/myStory.js
@@ -1,28 +1,34 @@
+const convAreas = [
+  { lat: 43.8519712, lng: -78.9583349 },
+  { lat: 43.9392489, lng: -78.9811295 },
+  { lat: 43.85359, lng: -78.898358 },
+  { lat: 44.009224, lng: -79.070157 },
+  { lat: 44.002296, lng: -78.774953 },
+  { lat: 43.9002, lng: -79.065092 },
+  { lat: 44.297645, lng: -78.270045 },
+  { lat: 44.321988, lng: -78.261056 },
+];
+let timedConvMarkers = [];
+
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 44.17, lng: -78.609 },
   });
 
-  const convAreas = [
-    [{ lat: 43.8519712, lng: -78.9583349 }, "Lynde Shores Conservation Area"],
-    [{ lat: 43.9392489, lng: -78.9811295 }, "Heber Down Conservation Area"],
-    [{ lat: 43.85359, lng: -78.898358 }, "Thicksons Woods"],
-    [{ lat: 44.009224, lng: -79.070157 }, "?"],
-    [{ lat: 44.002296, lng: -78.774953 }, "?"],
-    [{ lat: 43.9002, lng: -79.065092 }, "?"],
-    [{ lat: 44.297645, lng: -78.270045 }, "Mark S. Burnham Provincial Park"],
-    [{ lat: 44.321988, lng: -78.261056 }, "Harold Town Conservation Area"],
-  ];
-  // Create an info window to share between markers.
-  const infoWindow = new google.maps.InfoWindow();
-
+  for (let i = 0; i < convAreas.length; i++) {
+      timeMarkerFall(convAreas[i], i * 100);
+  }
   // Create the markers.
-  for (i = 0, i < convAreas.length; i++; ) {
-    const marker = new google.maps.Marker({
-      map,
-      animation: google.maps.Animation.DROP,
-      position: [i][1],
-    });
+  function timeMarkerFall(position, timeout){
+    window.setTimeout(() => {
+      timedConvMarkers.push(
+        new google.maps.Marker({
+          position: position,
+          map,
+          animation: google.maps.Animation.DROP,
+        })
+      );
+    }, timeout);
   }
 }
