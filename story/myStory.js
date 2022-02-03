e7e3475dcd3e769f8ee228d4169b37c6b46e3815
mokhy2 <alimokheam@outlook.com>
2022-02-02 21:05:45 -0500

    Added storyline path

--- story/myStory.js
+++ story/myStory.js
@@ -8,125 +8,41 @@ const convAreas = [
   { lat: 44.297645, lng: -78.270045 },
   { lat: 44.321988, lng: -78.261056 },
 ];
+
+storyLinePoints = [
+  { lat: 43.854852, lng: -78.966963 },
+  { lat: 43.9392489, lng: -78.9811295 },
+  { lat: 43.85359, lng: -78.898358 },
+  { lat: 44.009224, lng: -79.070157 },
+  { lat: 44.002296, lng: -78.774953 },
+  { lat: 43.9002, lng: -79.065092 },
+  { lat: 44.297645, lng: -78.270045 },
+  { lat: 44.321988, lng: -78.261056 },
+]
+
 let timedConvMarkers = [];
+let storyLinePath;
 
 const markerLabels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let markerLabelIndex = 0;
 
 
 function initMap() {
-  // Create a new StyledMapType object, passing it an array of styles,
-  // and the name to be displayed on the map type control.
-  const styledMapType = new google.maps.StyledMapType(
-    [
-      { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
-      { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
-      { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
-      {
-        featureType: "administrative",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#c9b2a6" }],
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#dcd2be" }],
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#ae9e90" }],
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "geometry",
-        stylers: [{ color: "#dfd2ae" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry.fill",
-        stylers: [{ color: "#a5b076" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#447530" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#f2eab4" }],
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "geometry",
-        stylers: [{ color: "#fdfcf8" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#f8c967" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#e9bc62" }],
-      },
-      {
-        featureType: "road.highway.controlled_access",
-        elementType: "geometry",
-        stylers: [{ color: "#e98d58" }],
-      },
-      {
-        featureType: "road.highway.controlled_access",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#db8555" }],
-      },
-      {
-        featureType: "road.local",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#806b63" }],
-      },
-      {
-        featureType: "transit.line",
-        elementType: "geometry",
-        stylers: [{ color: "#dfd2ae" }],
-      },
-      {
-        featureType: "transit.line",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#8f7d77" }],
-      },
-      {
-        featureType: "transit.line",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#ebe3cd" }],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "geometry",
-        stylers: [{ color: "#dfd2ae" }],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry.fill",
-        stylers: [{ color: "#a3c8e5" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#1e72e1" }],
-      },
-    ],
-    { name: "Styled Map" }
-  );
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 44.17, lng: -78.609 },
-    mapTypeControlOptions: {
-      mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain", "styled_map"],
-    },
   });
+
+  storyLinePath = new google.maps.Polyline({
+    path: storyLinePoints,
+    strokeColor: "#1fe058",
+    strokeOpacity: 0.8,
+    strokeWeight: 1.5,
+  });
+
+  document.getElementById("add-line").addEventListener("click", addLine);
+  document.getElementById("remove-line").addEventListener("click", removeLine);
+  addLine();
 
   for (let i = 0; i < convAreas.length; i++) {
       timeMarkerFall(convAreas[i], i * 100);
@@ -145,4 +61,12 @@ function initMap() {
     }, timeout);
   }
 
+}
+
+function addStoryPath() {
+  flightPath.setMap(map);
+}
+
+function removeStoryPath() {
+  flightPath.setMap(null);
 }
